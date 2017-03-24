c583e298d1db38c6849bb4d3ec893b0c44b19f2b
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-03-24 11:05:17 -0400

    Define __version__

--- obo/__init__.py
+++ obo/__init__.py
@@ -1,1 +1,3 @@
 from .read import read_obo
+
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
+import os
+import re
+
 import setuptools
+
+
+directory = os.path.dirname(os.path.abspath(__file__))
+
+# version
+init_path = os.path.join(directory, 'obo', '__init__.py')
+with open(init_path) as read_file:
+    text = read_file.read()
+pattern = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]", re.MULTILINE)
+version = pattern.search(text).group(1)
 
 setuptools.setup(
     name = 'obo',
-    version = '0.1.0',
+    version = version,
     author = 'Daniel Himmelstein',
     author_email = 'daniel.himmelstein@gmail.com',
     url = 'https://github.com/dhimmel/obo',
