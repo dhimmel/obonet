2d1b1648114d1158c52db45ea449c05aae1e9f73
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-03-24 11:07:34 -0400

    setup.py: convert README to reStructuredText

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 import re
+import subprocess
 
 import setuptools
 
@@ -13,6 +14,21 @@ with open(init_path) as read_file:
 pattern = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]", re.MULTILINE)
 version = pattern.search(text).group(1)
 
+# long_description
+readme_path = os.path.join(directory, 'README.md')
+try:
+    # Try to create an reStructuredText long_description from README.md
+    args = 'pandoc', '--from', 'markdown', '--to', 'rst', readme_path
+    long_description = subprocess.check_output(args)
+    long_description = long_description.decode()
+except Exception as error:
+    # Fallback to markdown (unformatted on PyPI) long_description
+    print('README.md conversion to reStructuredText failed. Error:')
+    print(error)
+    with open(readme_path) as read_file:
+        long_description = read_file.read()
+
+
 setuptools.setup(
     name = 'obo',
     version = version,
@@ -20,6 +36,7 @@ setuptools.setup(
     author_email = 'daniel.himmelstein@gmail.com',
     url = 'https://github.com/dhimmel/obo',
     description = 'OBO ontology tools in python',
+    long_description = long_description,
     license = 'CC0',
     packages = ['obo'],
     install_requires = ['networkx'],
