0cf3b4a3bd8c64df31419a19d551e15efffe32b2
Eric Torstenson <eric.s.torstenson@vumc.org>
2020-09-27 07:09:55 -0500

    read_obo ignore_obsolete option (#15)
    merges https://github.com/dhimmel/obonet/pull/15
    
    Add ability to include nodes marked as obsolete.
    Defaults to ignore_obsolete=True for backwards compatibility.
    
    Add obsolete node to tests/data/brenda-subset.obo (BTO:0000311)
    to allow to testing ignore_obsolete.
    
    Upgrade tests to use networkx.MultiDiGraph.nodes syntax for lookup,
    rather than the legacy .node syntax. Fixes failing tests, since graph.node
    syntax was removed in networkx 2.4.
    https://networkx.github.io/documentation/stable/release/release_2.4.html
    graph.nodes works for all networkx==2 releases. Skip some tests on networkx==1
    releases, since .nodes fails.

--- tests/test_obo_reading.py
+++ tests/test_obo_reading.py
@@ -8,17 +8,17 @@ from obonet.read import parse_tag_line
 
 directory = os.path.dirname(os.path.abspath(__file__))
 
-
 def test_read_taxrank_file():
     """
     Test reading the taxrank ontology OBO file.
     """
+    pytest.importorskip("networkx", minversion="2.0")
     path = os.path.join(directory, 'data', 'taxrank.obo')
     with open(path, 'rt') as read_file:
         taxrank = obonet.read_obo(read_file)
     assert len(taxrank) == 61
-    assert taxrank.node['TAXRANK:0000001']['name'] == 'phylum'
-    assert 'NCBITaxon:kingdom' in taxrank.node['TAXRANK:0000017']['xref']
+    assert taxrank.nodes['TAXRANK:0000001']['name'] == 'phylum'
+    assert 'NCBITaxon:kingdom' in taxrank.nodes['TAXRANK:0000017']['xref']
 
 
 @pytest.mark.parametrize('extension', ['', '.gz', '.bz2', '.xz'])
@@ -118,3 +118,20 @@ def test_parse_tag_line_backslashed_exclamation():
     tag, value, trailing_modifier, comment = parse_tag_line(line)
     assert tag == 'synonym'
     assert value == r'not a real example \!'
+
+def test_ignore_obsolete_nodes():
+    """Quick verification that the change doesn't break anything"""
+    path = os.path.join(directory, 'data', 'brenda-subset.obo')
+    brenda = obonet.read_obo(path)
+    nodes = brenda.nodes(data=True)
+    assert "BTO:0000311" not in nodes
+
+def test_presence_of_obsolete_nodes():
+    """Test that we did, indeed, capture those obsolete entries"""
+    pytest.importorskip("networkx", minversion="2.0")
+    path = os.path.join(directory, 'data', 'brenda-subset.obo')
+    brenda = obonet.read_obo(path, ignore_obsolete=False)
+    nodes = brenda.nodes(data=True)
+    assert "BTO:0000311" in nodes
+    node = nodes['BTO:0000311']
+    assert node['is_obsolete'] == 'true'
