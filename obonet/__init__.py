9aaa6631c9cfbbb734831972fa50a1a3639fb9d2
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2021-04-03 20:58:02 -0400

    packaging: setup.cfg, pyproject.toml, setuptools_scm

--- obonet/__init__.py
+++ obonet/__init__.py
@@ -1,3 +1,18 @@
 from .read import read_obo
 
-__version__ = "0.2.6"
+__all__ = [
+    "read_obo",
+]
+
+
+def _get_version():
+    # https://github.com/pypa/setuptools_scm#retrieving-package-version-at-runtime
+    from pkg_resources import DistributionNotFound, get_distribution
+
+    try:
+        return get_distribution("obonet").version
+    except DistributionNotFound:
+        return None
+
+
+__version__ = _get_version()
