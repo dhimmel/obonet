c0075ad1beed7a81902bf4084b568e84c631786f
Ben Gyori <ben.gyori@gmail.com>
2024-07-19 14:25:58 -0400

    Get version without deprecation warnings

--- obonet/__init__.py
+++ obonet/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import sys
 
 from .read import read_obo
 
@@ -9,12 +10,20 @@ __all__ = [
 
 def _get_version() -> str | None:
     # https://github.com/pypa/setuptools_scm#retrieving-package-version-at-runtime
-    from pkg_resources import DistributionNotFound, get_distribution
+    if sys.version_info >= (3, 8):
+        from importlib.metadata import PackageNotFoundError, version
 
-    try:
-        return str(get_distribution("obonet").version)
-    except DistributionNotFound:
-        return None
+        try:
+            return version("obonet")
+        except PackageNotFoundError:
+            return None
+    else:
+        from pkg_resources import DistributionNotFound, get_distribution
+
+        try:
+            return str(get_distribution("obonet").version)
+        except DistributionNotFound:
+            return None
 
 
 __version__ = _get_version()
