b0fdca8dd55537a4eed6524eb3436a922dc241ee
Ben Gyori <ben.gyori@gmail.com>
2024-07-19 16:18:16 -0400

    Make getting version Python 3.8+ compatible

--- obonet/__init__.py
+++ obonet/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import sys
 
 from .read import read_obo
 
@@ -9,21 +8,12 @@ __all__ = [
 
 
 def _get_version() -> str | None:
-    # https://github.com/pypa/setuptools_scm#retrieving-package-version-at-runtime
-    if sys.version_info >= (3, 8):
-        from importlib.metadata import PackageNotFoundError, version
+    from importlib.metadata import PackageNotFoundError, version
 
-        try:
-            return version("obonet")
-        except PackageNotFoundError:
-            return None
-    else:
-        from pkg_resources import DistributionNotFound, get_distribution
-
-        try:
-            return str(get_distribution("obonet").version)
-        except DistributionNotFound:
-            return None
+    try:
+        return version("obonet")
+    except PackageNotFoundError:
+        return None
 
 
 __version__ = _get_version()
